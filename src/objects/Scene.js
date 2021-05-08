6d3c829cb39ecf6f8bd4db496d8af35140f4defe
Dwarkesh Patel <dwarkesh.sanjay.patel@gmail.com>
2021-05-08 10:11:00 -0500

    workung

--- src/objects/Scene.js
+++ src/objects/Scene.js
@@ -1,16 +1,39 @@
-import { Group } from "three";
+import * as THREE from "three";
 
-import BasicLights from "./Lights.js";
+import Lights from "./Lights.js";
 import BlochSphere from "./BlochSphere";
+import Gate from "./Gate";
 
-export default class SeedScene extends Group {
+export default class SeedScene extends THREE.Group {
   constructor() {
     super();
 
-    const lights = new BasicLights();
     this.blochSphere = new BlochSphere();
 
-    this.add(lights, this.blochSphere);
+    this.gates = [
+      new Gate(
+        "Pauli X",
+        new THREE.Quaternion(0, 0, 1, 0),
+        new THREE.Vector3(-6, 0, -2.5)
+      ),
+      new Gate(
+        "Pauli Y",
+        new THREE.Quaternion(0, 1, 0, 0),
+        new THREE.Vector3(-2, 0, -2.5)
+      ),
+      new Gate(
+        "Pauli Z",
+        new THREE.Quaternion(1, 0, 0, 0),
+        new THREE.Vector3(2, 0, -2.5)
+      ),
+      new Gate(
+        "Hadamard",
+        new THREE.Quaternion(0, 1, 0, 1).normalize(),
+        new THREE.Vector3(6, 0, -2.5)
+      ),
+    ];
+
+    this.add(new Lights(), this.blochSphere);
   }
 
   update(timeStamp) {
